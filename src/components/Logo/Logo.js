4164fe244c9f75693dc4a77d60adb6d118b235cc
dimavasilyev <dima.vasilyev@bigdatafed.com>
2018-11-17 22:25:19 +0200

    Created Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css';
+
+function Logo(props) {
+  return (
+    <aside className="logo">
+      <span className="logo__text">H</span>
+    </aside>
+  );
+}
+
+export default Logo;
